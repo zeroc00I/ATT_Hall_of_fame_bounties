55bfb9848c0dc9f53d952bbeecf2a36b864fb4e2
button value=11 <b.minossi@gmail.com>
2024-08-16 16:34:18 -0300

    Update Total_amount_generator.sh

--- Total_amount_generator.sh
+++ Total_amount_generator.sh
@@ -1,2 +1,1 @@
-// after walking through all public graphql entries on hackerone dashboard
-cat all_att | jq -r '.[].search.nodes[]|"\(.reporter.username) \(.total_awarded_amount)"' | awk '{ if ($2 == "null") $2 = 0; sum[$1] += $2 } END { for (i in sum) print i, sum[i] }'
+cat all_att | jq -r '.[].search.nodes[]|"\(.reporter.username) \(.total_awarded_amount)"' | awk '{ if ($2 == "null") $2 = 0; sum[$1] += $2 } END { for (i in sum) print i, sum[i] }'  | sort -k2,2nr
